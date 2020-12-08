433277039c288ffda6f146b11b674815db2d8302
Radek Stibora <rstibora@gmail.com>
2020-12-08 21:20:14 +0100

    Add second star solution to day 12

--- src/year2019/day_12.rs
+++ src/year2019/day_12.rs
@@ -1,10 +1,10 @@
-use num::abs;
+use num::{abs, integer::lcm};
 use crate::aoc_error::{AocError, AocResult};
 
 pub fn first_star(input: &str) -> AocResult {
     let mut positions = parse_positions_from_input(input)?;
     let mut velocities: Vec<[i32; 3]> = vec![[0, 0, 0]; positions.len()];
-    simulate(1_000_000_000, &mut positions, &mut velocities);
+    simulate(1000, &mut positions, &mut velocities, 0..3);
 
     let system_energy = positions.iter().zip(velocities.iter())
                             .fold(0, |acc, (position, velocity)| {
@@ -14,27 +14,35 @@ pub fn first_star(input: &str) -> AocResult {
 }
 
 pub fn second_star(input: &str) -> AocResult {
-    let mut positions = parse_positions_from_input(input)?;
-    let mut velocities: Vec<[i32; 3]> = vec![[0, 0, 0]; positions.len()];
-    let original_velocities: Vec<[i32; 3]> = vec![[0, 0, 0]; positions.len()];
-    
-    let mut num_iterations: u64 = 0;
-    simulate(1, &mut positions, &mut velocities);
-    while positions != original_velocities {
-        num_iterations += 1;
-        simulate(1, &mut positions, &mut velocities);
-    }
+    let original_positions = parse_positions_from_input(input)?;
+    let original_velocities: Vec<[i32; 3]> = vec![[0, 0, 0]; original_positions.len()];
 
-    Ok(num_iterations.to_string())
+    let mut num_steps_per_dimension: [i64; 3] = [0; 3];
+    for dimension in 0..3 {
+        let mut positions = original_positions.to_owned();
+        let mut velocities = original_velocities.to_owned();
+
+        let mut num_steps = 1;
+        simulate(1, &mut positions, &mut velocities, dimension..dimension + 1);
+        num_steps_per_dimension[dimension] = loop {
+            while positions != original_positions || velocities != original_velocities {
+                num_steps += 1;
+                simulate(1, &mut positions, &mut velocities, dimension..dimension + 1);
+            }
+            break num_steps
+        }
+    }
+    let result = lcm(lcm(num_steps_per_dimension[0], num_steps_per_dimension[1]), num_steps_per_dimension[2]);
+    Ok(result.to_string())
 }
 
-pub fn simulate(iterations: u64, positions: &mut Vec<[i32; 3]>, velocities: &mut Vec<[i32; 3]>) {
+fn simulate(iterations: u64, positions: &mut Vec<[i32; 3]>, velocities: &mut Vec<[i32; 3]>, dimensions: std::ops::Range<usize>) {
     let num_moons = positions.len();
     for _ in 0..iterations {
         // Update velocities by applying gravity.
         for moon_a_index in 0..num_moons - 1 {
             for moon_b_index in moon_a_index + 1..num_moons {
-                for coordinate in 0..3 {
+                for coordinate in dimensions.to_owned() {
                     let distance_signum = num::signum(positions[moon_b_index][coordinate] - positions[moon_a_index][coordinate]);
                     velocities[moon_a_index][coordinate] += distance_signum;
                     velocities[moon_b_index][coordinate] -= distance_signum;
@@ -44,7 +52,7 @@ pub fn simulate(iterations: u64, positions: &mut Vec<[i32; 3]>, velocities: &mut
 
         // Update positions by applying velocities.
         for moon_index in 0..positions.len() {
-            for coordinate in 0..3 {
+            for coordinate in dimensions.to_owned() {
                 positions[moon_index][coordinate] += velocities[moon_index][coordinate];
             }
         }
